a32431074f4cadb72f5b9b4f0fdf746f79006129
pierrototo <pierre69marion@gmail.com>
2016-01-28 12:24:24 +0100

    ajout de commentaires

--- src/musicEvolution/Note.java
+++ src/musicEvolution/Note.java
@@ -10,25 +10,53 @@ package musicEvolution;
  * @author Gess
  */
 public class Note {
+    
+    /*********************************************
+    *** Définition des paramètres de la classe ***
+    *********************************************/
+    
     // Entier représentant une note
     private int id;
     
+    /**
+     * Constructeur permettant d'initialiser une note.
+     */
     public Note() {
         this.id =0;
     }
     
+    /**
+     * Constructeur permettant de créer une note en fonction
+     * d'une autre.
+     * 
+     * @param _id 
+     */
     public Note(int _id) {
         this.id =_id;
     }
     
+    /**
+     * Procédure permettant de choisir une note au hasard.
+     * 
+     */
     public void randomNote() {
         this.id = (int)(Math.random() * 127);
     }
 
+    /**
+     * getter id
+     * 
+     * @return 
+     */
     public int getId() {
         return id;
     }
-
+    
+    /**
+     * setter id
+     * 
+     * @param id 
+     */
     public void setId(int id) {
         this.id = id;
     }
